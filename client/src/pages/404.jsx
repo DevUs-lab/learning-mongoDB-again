80de4ed4dee23be168085e2a1dd668dfb1c56cfd
Umair Saeed <umairdevus@gmail.com>
2025-10-25 11:16:20 +0500

    add 404 page

---
+++ client/src/pages/404.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PageNotFound = () => {
+    return (
+        <section className='d-block min-vh-100'>
+            <div className="container">
+                <div className="row">
+                    <div className="col">dafa</div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default PageNotFound
--- client/src/pages/Router.jsx
+++ client/src/pages/Router.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Frontend from './Frontend'
 import Auth from './Auth'
+import PageNotFound from './404'
 
 const Index = () => {
     return (
         <Routes>
             <Route path="/*" element={<Frontend />} />
             <Route path='/auth/*' element={<Auth />} />
+            <Route path='*' element={<PageNotFound />} />
         </Routes>
     )
 }
